84e4c9325e29fb90d813d4e9e34be52468574be0
B-Moritz <bermorols@gmail.com>
2024-10-15 09:19:20 +0200

    REsolving problems with bind mount

--- run_current_experiment.bash
+++ run_current_experiment.bash
@@ -7,5 +7,5 @@ ls /app/logdata
 for pop_size in 150 200 250
 do
         echo 'Running with N=$pop_size'
-        python ./alg/NSGA_II/run_experiment.py -cn experiment_qga_position.yaml 'quantum_position_encoding={pop_object : {N : $pop_size}}
+        python ./alg/NSGA_II/run_experiment.py -cn experiment_qga_position.yaml "quantum_position_encoding={pop_object : {N : $pop_size}}"
 done
