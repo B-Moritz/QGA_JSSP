2061840fb8476af520663db864622130980337ad
B-Moritz <bermorols@gmail.com>
2024-10-15 09:03:42 +0200

    Creating experiment for rb5
    Resolved conflicts

--- run_current_experiment.bash
+++ run_current_experiment.bash
@@ -6,6 +6,7 @@ ls /app/logdata
 
 for pop_size in 150 200 250
 do
-        echo 'Running with N=$pop_size'
-        python ./alg/NSGA_II/run_experiment.py -cn experiment_qga_position.yaml "quantum_position_encoding={pop_object : {N : $pop_size}}"
+        echo 'Running with N=$pop_siz'
+        python ./alg/NSGA_II/run_experiment.py -cn experiment_classical.yaml "classical={pop_object : {N : $pop_size}}"
+
 done
