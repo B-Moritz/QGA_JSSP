b54b61ea924715536e68edc6dcf1d2218a899376
B-Moritz <bermorols@gmail.com>
2025-02-19 23:51:02 +0100

    Adding docker config for experiment 2

--- run_classical_experiment.bash
+++ run_classical_experiment.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ls /QGA_JSSP/logdata
+ls /QGA_JSSP/logdata_2
 
 #python ./alg/NSGA_II/run_experiment.py -cn experiment_qga_position.yaml
 
-for pop_size in 150 200 250
+for pop_size in 50
 do
         echo 'Running with N=$pop_size'
         python ./alg/NSGA_II/run_experiment.py -cn experiment_classical.yaml "classical={pop_object : {N : $pop_size}}"
--- run_hash_based.bash
+++ run_hash_based.bash
@@ -7,5 +7,5 @@ ls -la /QGA_JSSP/logdata_2
 for pop_size in 50
 do
         echo 'Running with N=$pop_size'
-        python ./alg/NSGA_II/run_experiment.py -cn experiment_qga_position.yaml "quantum_position_encoding_restricted={pop_object : {N : $pop_size}}"
+        python ./alg/NSGA_II/run_experiment.py -cn experiment_hash_based.yaml "quantum_hash_base_encoding={pop_object : {N : $pop_size}}"
 done
